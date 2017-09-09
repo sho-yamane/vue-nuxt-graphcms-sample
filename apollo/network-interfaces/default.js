63e12c14976a30986cc34db4a097260899493aa7
Sho Yamane <sns.yamane@gmail.com>
2017-09-09 10:01:06 +0900

    endpoint fixed

--- apollo/network-interfaces/default.js
+++ apollo/network-interfaces/default.js
@@ -3,7 +3,7 @@ import { createNetworkInterface } from 'apollo-client'
 export default (ctx) => {
   const networkInterface = createNetworkInterface({
     // GraphCMSのSimple Endpoint（Relay EndpointでもOK）を指定する
-    uri: 'https://api.graphcms.com/simple/v1/xxxxxx'
+    uri: 'https://api.graphcms.com/simple/v1/blogtest'
   })
   return networkInterface
 }
